7affdd7e862c2404ab561f7da1db6411ccdcb092
catfdo <a47504@alunos.ipb.pt>
2025-03-03 20:28:06 +0000

    createinputtocontrol2dcharactermovement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,35 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerControllerTutorialUpdates : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public InputAction LeftAction;
+// Start is called before the first frame update
+void Start()
+{
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        transform.position = position;
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
 
+}
+
+
+// Update is called once per frame
+void Update()
+{
+    float horizontal = 0.0f;
+    if (Keyboard.current.leftArrowKey.isPressed)
+    {
         horizontal = -1.0f;
-
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-
-            horizontal = 1.0f;
-
-        }~
-
-        Debug.Log(horizontal);
-    
+ 	    }
+    else if (Keyboard.current.rightArrowKey.isPressed)
+    {
+        horizontal = 1.0f;
     }
+    Debug.Log(horizontal);
+
+
+    float vertical = 0.0f;
+    if (Keyboard.current.upArrowKey.isPressed)
+    {
+        vertical = 1.0f;
+    }
+    else if (Keyboard.current.downArrowKey.isPressed)
+    {
+        vertical = -1.0f;
+    }
+    Debug.Log(vertical);
+
+
+    Vector2 position = transform.position;
+    position.x = position.x + 0.1f * horizontal;
+    position.y = position.y + 0.1f * vertical;
+    transform.position = position;
+}
+
 }
