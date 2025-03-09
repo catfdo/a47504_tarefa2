034319d2cf2ec0327324e25264fd000b44faa0ab
catfdo <a47504@alunos.ipb.pt>
2025-03-09 16:30:21 +0000

    setupabasichealthsystem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,12 @@ public class PlayerController : MonoBehaviour
   public InputAction MoveAction;
   Rigidbody2D rigidbody2d;
   Vector2 move;
+  public float speed = 3.0f;
+
+
+  // Variables related to the health system
+  public int maxHealth = 5;
+  int currentHealth;
 
 
   // Start is called before the first frame update
@@ -17,6 +23,7 @@ public class PlayerController : MonoBehaviour
   {
      MoveAction.Enable();
      rigidbody2d = GetComponent<Rigidbody2D>();
+     currentHealth = maxHealth;
   }
  
   // Update is called once per frame
@@ -27,8 +34,19 @@ public class PlayerController : MonoBehaviour
   }
 
 
-  void FixedUpdate()
+  // FixedUpdate has the same call rate as the physics system 
+ void FixedUpdate()
   {
-     Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+     Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
      rigidbody2d.MovePosition(position);
   }
+
+
+  void ChangeHealth (int amount)
+  {
+     currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+     Debug.Log(currentHealth + "/" + maxHealth);
+  }
+
+
+}
