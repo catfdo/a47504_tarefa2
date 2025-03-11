609dc4d185de3a9ecd322f1c3fbcbf459be41f2d
catfdo <a47504@alunos.ipb.pt>
2025-03-11 00:25:08 +0000

    create2dparticleeffects

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,31 +5,30 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
    // Public variables
-   public float speed;
-   public bool vertical;
-   public float changeTime = 3.0f;
-  
-   // Private variables
-   Rigidbody2D rigidbody2d;
-   Animator animator;
-   float timer;
-   int direction = 1;
-   bool broken = true;
+public float speed;
+public bool vertical;
+public float changeTime;
+public ParticleSystem smokeEffect;
+// Private variables
+Rigidbody2D rigidbody2d;
+Animator animator;
+float timer;
+int direction = 1;
+bool broken = true;
 
 
-   // Start is called before the first frame update
-   void Start()
-   {
-       rigidbody2d = GetComponent<Rigidbody2D>();
-       animator = GetComponent<Animator>();
-       timer = changeTime;
-      
-   }
+// Start is called before the first frame update
+void Start()
+{
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        timer = changeTime;
+}
 
 
- // Update is called every frame
-  void Update()
-  {
+// Update is called every frame
+void Update()
+{
         timer -= Time.deltaTime;
 
 
@@ -38,62 +37,51 @@ public class EnemyController : MonoBehaviour
             direction = -direction;
             timer = changeTime;
         }
-  }
+}
+
+// FixedUpdate has the same call rate as the physics system
+void FixedUpdate()
+{
+        if(!broken)
+        {
+                return;
+        }
+      
+        Vector2 position = rigidbody2d.position;
+        if (vertical)
+        {
+                position.y = position.y + speed * direction * Time.deltaTime;
+                animator.SetFloat("Move X", 0);
+                animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+                position.x = position.x + speed * direction * Time.deltaTime;
+                animator.SetFloat("Move X", direction);
+                animator.SetFloat("Move Y", 0);
+        }
+        rigidbody2d.MovePosition(position);
+}
 
 
-  // FixedUpdate has the same call rate as the physics system
-  void FixedUpdate()
-  {
-      if(!broken)
-      {
-           return;
-      }
-     
-       Vector2 position = rigidbody2d.position;
-     
-       if (vertical)
-       {
-           position.y = position.y + speed * direction * Time.deltaTime;
-           animator.SetFloat("Move X", 0);
-           animator.SetFloat("Move Y", direction);
-       }
-       else
-       {
-           position.x = position.x + speed * direction * Time.deltaTime;
-           animator.SetFloat("Move X", direction);
-           animator.SetFloat("Move Y", 0);
-       }
+void OnTriggerEnter2D(Collider2D other)
+{
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
 
 
-       rigidbody2d.MovePosition(position);
-  }
+        if (player != null)
+        {
+                player.ChangeHealth(-1);
+        }
+}
 
 
-   void OnTriggerEnter2D(Collider2D other)
-   {
-       PlayerController player = other.gameObject.GetComponent<PlayerController>();
-
-
-       if (player != null)
-       {
-           player.ChangeHealth(-1);
-       }
-   }
-
-
- void OnCollisionEnter2D(Collision2D collision)
-    {
-        Destroy(gameObject);
-    }
-
-
-
-   public void Fix()
-   {
-       broken = false;
-       GetComponent<Rigidbody2D>().simulated = false;
-       animator.SetTrigger("Fixed");
-   }
-
+public void Fix()
+{
+        broken = false;
+        rigidbody2d.simulated = false;
+        animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
+}
 
 }
